ec9263388cbf3fba1075a7b2a9e4eb338d39a99e
Divi22005 <23r11a66b8@gcet.edu.in>
2025-06-06 12:02:54 +0530

    updated

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from 'dotenv';
+import todoRouter from "./Routes/todoRoutes.js";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 const DBUSER = encodeURIComponent(process.env.DBUSER)
 const DBPASS = encodeURIComponent(process.env.DBPASS)
 const MONGO_URI =`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.e3jawml.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluster0`
+
+app.use("/", todoRouter);
 
 mongoose
   .connect(MONGO_URI)
